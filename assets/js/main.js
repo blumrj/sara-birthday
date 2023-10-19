1f5cf9cd750074c1592a87a5bdcf0a85eb263670
blumrj <marijaf202@gmail.com>
2023-10-19 13:09:27 +0200

    working on sunset

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,37 @@
 $(function() {
     $.scrollify({
         section : ".scrollify-container",
-        scrollSpeed: 300,
+        scrollSpeed: 400,
     });
 });
 
 AOS.init();
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    centeredSlides: true,
+    spaceBetween: 10,
+    breakpoints: {
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+            spaceBetween: 20
+        },
+        640: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+            spaceBetween: 30
+        }
+      },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
+  
