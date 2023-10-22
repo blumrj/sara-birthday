b090d3af26eefa833f6a4a589e876008bac29ff5
blumrj <marijaf202@gmail.com>
2023-10-22 23:02:02 +0200

    adding slides and cropping pics

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,12 +7,37 @@ $(function() {
 
 AOS.init();
 
-const swiper = new Swiper('.swiper', {
+const paintingsSwiper = new Swiper("#paintings-swiper", {
+    loop: true,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    // centeredSlides: true,
+    spaceBetween: 10,
+    breakpoints: {
+        // when window width is >= 480px
+        // 480: {
+        //     slidesPerView: 2,
+        //     slidesPerGroup: 2,
+        //     spaceBetween: 20
+        // },
+        640: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+            spaceBetween: 30
+        }
+      },
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+})
+
+const swiper = new Swiper('#sunset-swiper', {
     // Optional parameters
     loop: true,
     slidesPerView: 1,
     slidesPerGroup: 1,
-    centeredSlides: true,
     spaceBetween: 10,
     breakpoints: {
         // when window width is >= 480px
@@ -34,4 +59,56 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
 });
+
+const facetimeSwiper = new Swiper("#facetime-swiper", {
+    // loop: true,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    // centeredSlides: true,
+    spaceBetween: 10,
+    breakpoints: {
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+            spaceBetween: 20
+        },
+        640: {
+            slidesPerView: 5,
+            slidesPerGroup: 1,
+            spaceBetween: 30
+        }
+      },
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+})
+
+const notesSwiper = new Swiper("#notes-swiper", {
+    loop: true,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    // centeredSlides: true,
+    spaceBetween: 10,
+    breakpoints: {
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+            spaceBetween: 20
+        },
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        }
+      },
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+})
+
   
